359b624ec38497839832c874a587a71b9433bbaf
¨ABDERAHMEN609¨ <abderahmenyoussfi22@gmail.com>
2025-04-24 13:23:21 +0200

    first commit

---
+++ vehicle.js
@@ -0,0 +1,16 @@
+// Implement the Car class that implements the Vehicle interface
+var Car = /** @class */ (function () {
+    function Car(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    Car.prototype.start = function () {
+        console.log("Car engine started");
+    };
+    return Car;
+}());
+// Create an instance of the Car class
+var myCar = new Car("Toyota", "Corolla", 2020);
+// Call the start method on the instance
+myCar.start(); // This should log "Car engine started" to the console
